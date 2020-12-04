cee76f56fe053a109653f1506319941e02bd40f3
Loek Meijers <loek.e.meijers@gmail.com>
2020-12-04 17:08:54 +0100

    Added mean clustering coefficient function, fitting function and finished average neighbour degree function

--- network.py
+++ network.py
@@ -1,4 +1,4 @@
-#%%
+# %%
 import time
 import matplotlib.cm as cm
 import matplotlib.pyplot as plt
@@ -6,6 +6,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from scipy import sparse
+from scipy.optimize import curve_fit
+from scipy.stats import poisson
+from math import factorial
 # from collections import Counter
 
 #################### TO DO ##########################################
@@ -17,10 +20,6 @@ from scipy import sparse
 # bonus: estimation of fit to poisson (random graph) and power law (scale free graph)
 
 ######################################################################
-
-
-
-
 
 
 # i) choose network ===============================================================================================
@@ -35,9 +34,8 @@ class NetworkBuilder:
     def __init__(self, data_file):
         self._data_file = data_file
 
-
     def buildFromData(self):
-        
+
         # open file
         with open(self._data_file + ".txt", "r") as data:
             for i in range(4):
@@ -45,10 +43,8 @@ class NetworkBuilder:
             adjacency_list = data.readlines()
         print("\n-- file imported")
 
-
         length = len(adjacency_list)
         print("\n-- length adjacency list: " + str(length))
-
 
         # getting indices of non-zero items
         rowind = []
@@ -70,7 +66,6 @@ class NetworkBuilder:
         print("matrix size: " + str(self._A_size) + "^2")
         print(str(self._A[:6, :6]) + "\n")
 
-
         # create sparse matrix
         sparsevaluelist = [1] * (length * 1)  # vector of ones
 
@@ -86,11 +81,10 @@ class NetworkBuilder:
         sparse.save_npz(str(self._data_file) + "-A_sparse.npz", self._A_sparse)
         print("\n-- exported as npz")
 
-        # 
+        #
         #
 
         return Network(self._A, self._A_sparse, self._A_size, self._DegreeList)
-
 
     def buildByImport(self):
 
@@ -103,11 +97,10 @@ class NetworkBuilder:
         self._DegreeList = np.add.reduce(self._A)
         print("\n-- degree list created: \n" + str(self._DegreeList))
 
-        # 
         #
-        
-        return Network(self._A, self._A_sparse, self._A_size, self._DegreeList)
+        #
 
+        return Network(self._A, self._A_sparse, self._A_size, self._DegreeList)
 
 
 ########################################################################################################################
@@ -132,132 +125,156 @@ class Network:
 
         # plt.spy(self.A, precision=0)
 
-
-
     # iii) average clustering coefficient ===============================================================================================
 
-    def Matrix_Multiply(self):
+    def GetMeanC(self):
         """
-        Here we try to obtain the clustering coefficient by multiplying the adjacency matrix A with itself,
-        and then taking the multiplication A * A^2 only for those rows and columns that will form the diagonal of
-        A^3, since those are the only values form A^3 that give the amount of closed paths of length 3 (i.e. triangles)
-        The matrix multiplication method that is used here is just the standard numpy one. We need to find a better one.
+       Function to obtain clustering coefficient
         """
+
         diagonal = []
-        print(self.A[1, 100])
-        matrix2 = np.matmul(self.A, self.A)
+        A2_sparse = self.A_sparse @ self.A_sparse
+        A3_sparse = A2_sparse @ self.A_sparse
+        print("Matrix multiplication done.")
+        A3_dense = A3_sparse.toarray()
 
         for i in range(self.A_size):
-            value = 0
-            for j in range(self.A_size):
-                if (i != j):
-                    continue
-                for k in range(self.A_size):
-                    print(matrix2[i, k])
-                    value = value + self.A[i, k] * matrix2[k, i]
-            diagonal.append(value)
+            diagonal.append(A3_dense[i, i])
 
-        return diagonal
+        print("Filled the diagonal list.")
+        C = 0
 
+        for i in range(self.A_size):
+            #print("diagonal has value ", diagonal[i], "while degreelist has value ", self.DegreeList[i])
+            if diagonal[i] == 0:
+                continue
+            C = C + diagonal[i]/(self.DegreeList[i]*(self.DegreeList[i] - 1))
 
+        C_mean = C/self.A_size
+
+        print("Mean clustering was found to be %lf" % (C_mean))
 
     # iv) degree distribution ===============================================================================================
 
     def plotDegDis(self, style):
         # DegreeDistribution dd
         # count how many nodes have 1, 2, 3 .. n degrees
-        dd = [[degree, self.DegreeList.tolist().count(degree)] for degree in set(self.DegreeList)] # DegreeList is actually numpy.ndarray
+        dd = [[degree, self.DegreeList.tolist().count(degree)] for degree in set(
+            self.DegreeList)]  # DegreeList is actually numpy.ndarray
         # split into two variables for plotting
         x = [dd[i][0] for i in range(len(dd))]
         y = [dd[i][1] for i in range(len(dd))]
-
-
+        weight = np.add.reduce(y)
+        y_weighted = [float(i/weight) for i in y]
 
         if style == 'linear':
             # plt.subplot(3, 1, 1)        # divides into 2 rows & 1 column, uses location 1 (ie top row)
-            plt.bar(x[:49], y[:49])
+            plt.bar(x[:49], y_weighted[:49])
             plt.title('Linear relationship')
             # plt.legend(loc = 'best')
             plt.xlabel('degrees (<=50)')
             plt.ylabel('number of nodes')
             plt.grid()                  # show grid
-            plt.savefig('lin_plot.png', dpi = 200, transparent = True)
+            plt.savefig('lin_plot.png', dpi=200, transparent=True)
             plt.show()
 
         elif style == 'loglog':
             # plt.subplot(3,1,3)
-            plt.plot(np.log(x), np.log(y), 'ko')
+            plt.plot(np.log(x), np.log(y_weighted), 'ko')
             plt.title('Log-log relationship')
             # plt.legend(loc = 'best')
             plt.xlabel('log(degrees)')
             plt.ylabel('log(number of nodes)')
-            plt.savefig('loglog_plot.png', dpi = 200, transparent = True)
+            plt.savefig('loglog_plot.png', dpi=200, transparent=True)
             plt.show()
-
-
-
-
 
     # v) average neighbour degree ===============================================================================================
 
     def AverageNeighbourDegree(self):
         randNode = np.random.randint(0, self.A_size)
 
-        neighbours = np.where(self.A[randNode, :] == 1)[0]                    # returns tuple
+        neighbours = np.where(self.A[randNode, :] == 1)[
+            0]                    # returns tuple
+
+        Mean_Degree = 0
+
+        for i in range(self.A_size):
+            neighbours = np.where(self.A[i, :] == 1)[0]
+
+            Mean_Degree = Mean_Degree + \
+                round(np.add.reduce([self.DegreeList[j]
+                                     for j in neighbours])/len(neighbours), 2)
+        Mean_Degree = Mean_Degree/self.A_size
 
         # what about a list comprehension?
         # dont think its possible without using different data format, eg pandas Series or DataFrame
 
+        #print("Node %d has %d neighbours (double-check: %d), which are located at %s." % (randNode, len(neighbours), self.DegreeList[randNode], neighbours))
 
-        print("Node %d has %d neighbours (double-check: %d), which are located at %s." % (randNode, len(neighbours), self.DegreeList[randNode], neighbours))
+        #print("\nThose neighbours themself have on average %s neighbours." % round(np.add.reduce([self.DegreeList[i] for i in neighbours])/len(neighbours), 2))
 
-        print("\nThose neighbours themself have on average %s neighbours." % round(np.add.reduce([self.DegreeList[i] for i in neighbours])/len(neighbours), 2))
+        print("\nThe average amount of neighbour's neighbours is %s." %
+              round((Mean_Degree), 3))
 
-        print("\nThe average number of degrees in the entire network is %s." % round(np.add.reduce(self.DegreeList)/len(self.DegreeList), 3))
+        print("\nThe average number of degrees in the entire network is %s." %
+              round(np.add.reduce(self.DegreeList)/len(self.DegreeList), 3))
+
+    # vii & viii) Fitting to Poisson and power law ===============================================================================
+
+    def Fitting(self):
+
+        dd = [[degree, self.DegreeList.tolist().count(degree)] for degree in set(
+            self.DegreeList)]  # DegreeList is actually numpy.ndarray
+        # split into two variables for plotting
+        x = [dd[i][0] for i in range(len(dd))]
+        y = [dd[i][1] for i in range(len(dd))]
+        weight = np.add.reduce(y)
+        y_weighted = [float(i/weight) for i in y]
+
+        def Poisson(k, lamb):
+            return poisson.pmf(k, lamb)
 
 
+        def Powerlaw(x, a, b):
+            return a*x**b
 
 
+        params_poisson, c_poisson = curve_fit(Poisson, x, y_weighted)
+
+        lamb = params_poisson
+
+        params_power, c_power = curve_fit(Powerlaw, x, y_weighted)
+
+        a, b = params_power
+        
+        f1 = plt.plot(x, Poisson(x, lamb))
+        axes = plt.axes()
+        axes.set_xlim([0,50])
+        f1.show()
+
+        f2 = plt.plot(x, Powerlaw(x, a, b))
+        f2.show()
+        
 
 
-
-
-#%% run once ===============================================================================================
+# %% run once ===============================================================================================
 n1 = NetworkBuilder("enron")
 N1 = n1.buildFromData()
 
-
-
-#%% run afterwards by importing ===============================================================================================
+# %% run afterwards by importing ===============================================================================================
 n1 = NetworkBuilder("enron")
 N1 = n1.buildByImport()
-
 # N1.visualise()
 
+# %% iii) clustering coefficient ===============================================================================================
+N1.GetMeanC()
 
-
-
-#%% iii) clustering coefficient ===============================================================================================
-pass
-
-
-
-
-
-
-
-
-#%% iv) probability mass function ===============================================================================================
+# %% iv) probability mass function ===============================================================================================
 N1.plotDegDis('linear')
 N1.plotDegDis('loglog')
 
-
-
-
-
-
-#%% v) avg degree of neighbours ===============================================================================================
+# %% v) avg degree of neighbours ===============================================================================================
 N1.AverageNeighbourDegree()
 
-
-
+# %% vii & viii) Fitting to Poisson and power law
+N1.Fitting()
