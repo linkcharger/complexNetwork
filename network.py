2e9fe6a1275cee1abeeb99b1598942b7e706dc4a
Aaron Schade <as.tro.freak0@gmail.com>
2020-12-07 23:24:10 +0100

    clean up & all graphics

--- network.py
+++ network.py
@@ -1,20 +1,14 @@
 # %%
 import time
+from math import exp, factorial
+
 import matplotlib.cm as cm
 import matplotlib.pyplot as plt
-# %matplotlib qt
 import numpy as np
 import pandas as pd
 from scipy import sparse
 from scipy.optimize import curve_fit
-from scipy.stats import poisson
-from scipy.stats import chisquare
-from math import factorial
-from math import exp
-#from collections import Counter
-
-
-
+from scipy.stats import chisquare, poisson
 
 # To see whether everything would work on a different network, I tried this one: https://snap.stanford.edu/data/ca-AstroPh.html
 # I think there's a problem with the renumbering. When trying to find the mean C for the astro graph, there are about 10 nodes which say they have 1 closed triangle, with only 1 neighbour. This is impossible.
@@ -39,24 +33,8 @@ from math import exp
 
 # ii) adjacency matric A ===============================================================================================
 
-def renumber(old_list):
-    """
-    This function removes all duplicates from a list and then sorts from lowest to highest number.
-    Subsequently, the numbers in old_list are replaced with numbers from 0 to n, depending on where they are in the 
-    ranking from lowest to highest number.
-    """
-    new_list = old_list
-    no_duplicates = sorted(set(old_list))
-    length = len(no_duplicates)
-    no_duplicates = np.array(no_duplicates)
-
-    for a in range(length):
-        print("Now replacing the number of node %d of %d.\n" %(a, length))
-        for n, i in enumerate(new_list):
-            if i == no_duplicates[a]:
-                new_list[n] = a
-                
-    return new_list
+########################################################################################################################
+########################################################################################################################
 
 # creational pattern: builder pattern
 class NetworkBuilder:
@@ -95,7 +73,6 @@ class NetworkBuilder:
         for i in range(length):
             rowindex = rowind[i]
             columnindex = colind[i]
-
             # setting non-zero items in full matrix
             self._A[rowindex][columnindex] = 1
         print("\n-- full matrix created")
@@ -114,17 +91,17 @@ class NetworkBuilder:
         print("\n-- degree list created: \n" + str(self._DegreeList))
 
         # export
-        sparse.save_npz(str(self._data_file) + "-A_sparse.npz", self._A_sparse)
+        sparse.save_npz(str(self._data_file) + " - A_sparse.npz", self._A_sparse)
         print("\n-- exported as npz")
 
         #
         #
 
-        return Network(self._A, self._A_sparse, self._A_size, self._DegreeList)
+        return Network(self._data_file, self._A, self._A_sparse, self._A_size, self._DegreeList)
 
     def buildByImport(self):
 
-        self._A_sparse = sparse.load_npz(self._data_file + "-A_sparse.npz")
+        self._A_sparse = sparse.load_npz(self._data_file + " - A_sparse.npz")
         print("\n-- A_sparse imported from npz")
         self._A = self._A_sparse.toarray()
         print("\n-- A created from A_sparse")
@@ -136,7 +113,7 @@ class NetworkBuilder:
         #
         #
 
-        return Network(self._A, self._A_sparse, self._A_size, self._DegreeList)
+        return Network(self._data_file, self._A, self._A_sparse, self._A_size, self._DegreeList)
 
 
 ########################################################################################################################
@@ -144,24 +121,20 @@ class NetworkBuilder:
 
 class Network:
 
-    def __init__(self, A, A_sparse, A_size, DegreeList):
+    def __init__(self, name, A, A_sparse, A_size, DegreeList):
+        self.name = name
         self.A = A
         self.A_sparse = A_sparse
         self.A_size = A_size
         self.DegreeList = DegreeList
 
-    # def visualise(self):
-        # thought that would be nice to have for the project documentation in the end..
-        # maybe now it will work with the sparse matrix
+    def showA(self):
+        plt.figure(figsize = (8,8))
+        plt.title(self.name + " - adjacency matrix")
+        plt.spy(self.A_sparse, markersize = .5)
+        plt.savefig(self.name + ' - plot_' +  'A.png', dpi=200)
 
-        # fig = plt.figure()
-        # ax = fig.add_subplot(121)
-        # ax.imshow(self.A, interpolation='bilinear', cmap=cm.Greys_r)
-        # plt.show()
-
-        # plt.spy(self.A, precision=0)
-
-    # iii) average clustering coefficient ===============================================================================================
+# iii) average clustering coefficient ===============================================================================================
 
     def GetMeanC(self):
         """
@@ -176,10 +149,9 @@ class Network:
 
         for i in range(self.A_size):
             diagonal.append(A3_dense[i, i])
-
         print("Filled the diagonal list.")
-        C = 0
 
+        C = 0
         for i in range(self.A_size):
             #print("diagonal has value ", diagonal[i], "while degreelist has value ", self.DegreeList[i])
             if self.DegreeList[i] == 1 & diagonal[i] != 0:
@@ -193,13 +165,12 @@ class Network:
 
         print("Mean clustering was found to be %lf" % (C_mean))
 
-    # iv) degree distribution ===============================================================================================
+# iv) degree distribution ===============================================================================================
 
     def plotDegDis(self, style):
         # DegreeDistribution dd
         # count how many nodes have 1, 2, 3 .. n degrees
-        dd = [[degree, self.DegreeList.tolist().count(degree)] for degree in set(
-            self.DegreeList)]  # DegreeList is actually numpy.ndarray
+        dd = [[degree, self.DegreeList.tolist().count(degree)] for degree in set(self.DegreeList)]  # DegreeList is actually numpy.ndarray
         # split into two variables for plotting
         x = [dd[i][0] for i in range(len(dd))]
         y = [dd[i][1] for i in range(len(dd))]
@@ -207,33 +178,30 @@ class Network:
         y_weighted = [float(i/weight) for i in y]
 
         if style == 'linear':
-            # plt.subplot(3, 1, 1)        # divides into 2 rows & 1 column, uses location 1 (ie top row)
+            plt.figure(figsize = (10,6))        
             plt.bar(x[:49], y_weighted[:49])
-            plt.title('Linear relationship')
-            # plt.legend(loc = 'best')
+            plt.title(self.name + ' - Degree distribution, linear scale')
             plt.xlabel('degrees (<=50)')
             plt.ylabel('number of nodes')
-            plt.grid()                  # show grid
-            plt.savefig('lin_plot.png', dpi=200, transparent=True)
+            plt.savefig(self.name + ' - plot_DD_lin.png', dpi=200, transparent=True)
             plt.show()
 
         elif style == 'loglog':
-            # plt.subplot(3,1,3)
+            plt.figure(figsize = (10,6)) 
             plt.plot(np.log(x), np.log(y_weighted), 'ko')
-            plt.title('Log-log relationship')
-            # plt.legend(loc = 'best')
+            plt.title(self.name + ' - Degree distribution, loglog scale')
+            plt.grid()                                          # show grid
             plt.xlabel('log(degrees)')
             plt.ylabel('log(number of nodes)')
-            plt.savefig('loglog_plot.png', dpi=200, transparent=True)
+            plt.savefig(self.name + ' - plot_DD_loglog.png', dpi=200, transparent=True)
             plt.show()
 
-    # v) average neighbour degree ===============================================================================================
+# v) average neighbour degree ===============================================================================================
 
     def AverageNeighbourDegree(self):
         randNode = np.random.randint(0, self.A_size)
 
-        neighbours = np.where(self.A[randNode, :] == 1)[
-            0]                    # returns tuple
+        neighbours = np.where(self.A[randNode, :] == 1)[0]                    # returns tuple
 
         Mean_Degree = 0
 
@@ -258,14 +226,13 @@ class Network:
         print("\nThe average number of degrees in the entire network is %s." %
               round(np.add.reduce(self.DegreeList)/len(self.DegreeList), 3))
 
-    # vii & viii) Fitting to Poisson and power law ===============================================================================
+# vii & viii) Fitting to Poisson and power law ===============================================================================
 
     def Fitting(self):
 
-        #Maybe we should combine this with the plotdegdis function, since we actually need everything that's in there for this function
+        # Maybe we should combine this with the plotdegdis function, since we actually need everything that's in there for this function
 
-        dd = [[degree, self.DegreeList.tolist().count(degree)] for degree in set(
-            self.DegreeList)]  # DegreeList is actually numpy.ndarray
+        dd = [[degree, self.DegreeList.tolist().count(degree)] for degree in set(self.DegreeList)]  # DegreeList is actually numpy.ndarray
         # split into two variables for plotting
         x = [dd[i][0] for i in range(len(dd))]
         y = [dd[i][1] for i in range(len(dd))]
@@ -314,49 +281,96 @@ class Network:
         print(chisq1)
         print(chisq2)
         
+        
+########################################################################################################################
+########################################################################################################################
+
+def renumber(old_list):
+    """
+    This function removes all duplicates from a list and then sorts from lowest to highest number.
+    Subsequently, the numbers in old_list are replaced with numbers from 0 to n, depending on where they are in the 
+    ranking from lowest to highest number.
+    """
+    new_list = old_list
+    no_duplicates = sorted(set(old_list))
+    length = len(no_duplicates)
+    no_duplicates = np.array(no_duplicates)
+
+    for a in range(length):
+        print("Now replacing the number of node %d of %d.\n" %(a, length))
+        for n, i in enumerate(new_list):
+            if i == no_duplicates[a]:
+                new_list[n] = a
+                
+    return new_list
 
 
-# %% run once ===============================================================================================
-n1 = NetworkBuilder("enron")
+########################################################################################################################
+########################################################################################################################
+
+
+# %% ENRON  ===============================================================================================
+n1 = NetworkBuilder("Enron emails")
 N1 = n1.buildFromData()
+N1.showA()
 
-# %% run afterwards by importing ===============================================================================================
-n1 = NetworkBuilder("enron")
+# %% run afterwards by importing 
+n1 = NetworkBuilder("Enron emails")
 N1 = n1.buildByImport()
-# N1.visualise()
+N1.showA()
 
-# %% iii) clustering coefficient ===============================================================================================
+
+
+
+# %% iii) clustering coefficient 
 N1.GetMeanC()
 
-# %% iv) probability mass function ===============================================================================================
+# %% iv) probability mass function 
 N1.plotDegDis('linear')
 N1.plotDegDis('loglog')
 
-# %% v) avg degree of neighbours ===============================================================================================
+# %% v) avg degree of neighbours 
 N1.AverageNeighbourDegree()
 
-# %% vii & viii) Fitting to Poisson and power law ====================================================================
+# %% vii & viii) Fitting to Poisson and power law 
 N1.Fitting()
 
-# %% Run once ========================================================================
 
-n2 = NetworkBuilder("Astro", "renumber")
+
+
+
+
+# %% astrophysics citations =============================================================================
+n2 = NetworkBuilder("Astrophysics citations", "renumber")
 N2 = n2.buildFromData()
+N2.showA()
 
-# %% run afterwards by importing ========================================================================
-
-n2 = NetworkBuilder("Astro", "renumber")
+# %% run afterwards by importing 
+n2 = NetworkBuilder("Astrophysics citations")
 N2 = n2.buildByImport()
-# %% iii) clustering coefficient =====================================================================
+N2.showA()
+
+# %% iii) clustering coefficient 
 N2.GetMeanC()
 
-# %% iv) probability mass function ============================================================================
+# %% iv) probability mass function 
 N2.plotDegDis('linear')
 N2.plotDegDis('loglog')
 
-# %% v) avg degree of neighbours ===========================================================================
+# %% v) avg degree of neighbours 
 N2.AverageNeighbourDegree()
-# %% vii & viii) Fitting to Poisson and power law =======================================================
+# %% vii & viii) Fitting to Poisson and power law 
 N2.Fitting()
 
-# %%
+# %% just graphics
+n1 = NetworkBuilder("Enron emails")
+N1 = n1.buildByImport()
+N1.showA()
+N1.plotDegDis('linear')
+N1.plotDegDis('loglog')
+
+n2 = NetworkBuilder("Astrophysics citations")
+N2 = n2.buildByImport()
+N2.showA()
+N2.plotDegDis('linear')
+N2.plotDegDis('loglog')
